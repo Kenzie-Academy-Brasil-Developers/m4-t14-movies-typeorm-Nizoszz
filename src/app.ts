4fea2e97c5bfdeff8a98873a3df2d0b06488b707
Andrew <andrewairamdasilva@gmail.com>
2023-05-19 15:15:09 -0300

    feat: insert swagger docs

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import "express-async-errors";
 import express, { Application } from "express";
 import { handleErrors } from "./errors";
 import movieRoutes from "./routers/movie.routes";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app: Application = express();
 app.use(express.json());
 
 app.use("/movies", movieRoutes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(handleErrors);
 
